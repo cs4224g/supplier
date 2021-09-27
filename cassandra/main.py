c1fc57757983581ca0e1308f2c23fe1294bf4224
liawsy <liawsy99@gmail.com>
2021-09-27 22:40:13 +0800

    Implement T4

--- cassandra/main.py
+++ cassandra/main.py
@@ -1,30 +1,29 @@
+import sys
+
 from cassandra.query import named_tuple_factory, BatchStatement, SimpleStatement
 
 from cassandra.cluster import Cluster
+
+from transactions.t4 import execute_t4
+from transactions.t5 import execute_t5
 
 
 if __name__ == '__main__':
     # For connecting to multiple clusters
     # cluster = Cluster(['192.168.1.1', '192.168.1.2'])
     cluster = Cluster(['127.0.0.1'])
-    session = cluster.connect('test')
+    session = cluster.connect('wholesale_supplier')
     session.row_factory = named_tuple_factory
 
+    for line in sys.stdin:
+        input_arr = line.split(",")
+        xact = input_arr[0]
+        # TODO: Add respective if statements for other Xact types
+        if (xact == 'O'):
+            execute_t4(session, line)
+        elif (xact == 'S'):
+            execute_t5(session, line)
 
-    insert_stmt = "INSERT INTO test.example (W_ID, W_NAME, W_CATEGORY) VALUES (%s, %s, %s)"
-
-    # Executes atomically
-    batch = BatchStatement()
-    batch.add(SimpleStatement(insert_stmt), (1, "kitty1", "cat"))
-    batch.add(SimpleStatement(insert_stmt), (2, "kitty2", "cat"))
-    batch.add(SimpleStatement(insert_stmt), (3, "doggo1", "dog"))
-    session.execute(batch)
-
-    rows = session.execute("SELECT W_ID, W_NAME, W_CATEGORY FROM test.example LIMIT 2")
-
-    for r in rows:
-        print(r)
-        print(r.w_id, r.w_name, r.w_category)
 
 
     cluster.shutdown()
