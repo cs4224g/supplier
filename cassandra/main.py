ee129961aa5acf9d2df5266552cef85ee6ac1b3c
Pang Jia Da <pangjiada52@gmail.com>
2021-10-07 15:46:32 +0800

    main.py: add case for T1 and fall through

--- cassandra/main.py
+++ cassandra/main.py
@@ -4,7 +4,9 @@ from cassandra.query import named_tuple_factory, BatchStatement, SimpleStatement
 
 from cassandra.cluster import Cluster
 
+from transactions.t1 import execute_t1
 from transactions.t2 import execute_t2
+from transactions.t3 import execute_t3
 from transactions.t4 import execute_t4
 # from transactions.t5 import execute_t5
 
@@ -18,7 +20,7 @@ if __name__ == '__main__':
 
     for line in sys.stdin:
         # careful, the last argument will have \n. Affects string comparisons.
-        input_arr = line.split(",")
+        input_arr = line.split(",") 
         xact = input_arr[0]
         # TODO: Add respective if statements for other Xact types
         if (xact == 'O'):
@@ -27,6 +29,10 @@ if __name__ == '__main__':
             execute_t2(session, input_arr)
         elif(xact == 'D'):
             execute_t3(session, input_arr)
+        elif(xact == 'N'):
+            execute_t1(session, input_arr)
+        else:
+            print('fall thru', xact)
         # elif (xact == 'S'):
         #     execute_t5(session, line)
 
