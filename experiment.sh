3bb063d95ff42825d807761a5a10eb5a223f6f5e
liawsy <liawsy99@gmail.com>
2021-10-24 23:24:38 +0800

    Update script

--- experiment.sh
+++ experiment.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 allocate_server() {
-    experiment_type=$1
+    experiment_type=$2
     echo "$experiment_type"
     for ((i=0; i<40; i++))
     do
@@ -8,40 +8,32 @@ allocate_server() {
         echo "Assigning $machine_name to client $i..."
         file_name="xact_files_$experiment_type/$i.txt"
         echo $file_name
-        stats="$i,1,2,3,4,5,6,7"
         # -q to suppress banner
         ssh -q $machine_name "cd /temp/cs4224-group-g-cassandra && \
-        python3 main.py < $file_name && \
-        2> $i.csv && \ 
-        scp -q $i.csv cs4224g@xcnd40:/temp/cs4224-group-g-cassandra/stats && \
-        rm $i.csv"
-        # run_client $experiment_type $i
+        nohup ./run_client.sh $experiment_type $i &>/dev/null &"
     done
-    echo "sleeping for 5 seconds..."
-    sleep 5
-
-    touch "stats/client.csv"
-    for ((i=0; i<40; i++))
-    do
-        cat "stats/$i.csv" >> "stats/client.csv"
-        rm "stats/$i.csv"
-    done
-    # just in case not sorted, we sort each line by number (instead of string, so that 2 will come before 10,11,...)
-    sort -n "stats/client.csv" > "stats/client_sorted.csv"
-    mv "stats/client_sorted.csv" "stats/client.csv" 
-    echo "Stats printed to: stats/client.csv" 
-}
-
-run_client() {
-    echo "Running client program now..."
-    file_name="xact_files_$1/c$2.txt"
-    echo "$file_name"
 }
 
 get_overall_stats() {
     cd stats
-    python3 throughput.py 
+    touch "client.csv"
+    for ((i=0; i<40; i++))
+    do
+        cat "$i.csv" >> "client.csv"
+        rm "$i.csv"
+    done
+    # just in case not sorted, we sort each line by number (instead of string, so that 2 will come before 10,11,...)
+    sort -n "client.csv" > "client_sorted.csv"
+    mv "client_sorted.csv" "client.csv" 
+    echo "Stats printed to: client.csv"
+    python3 throughput.py
 }
 
 echo "Starting shell script..."
-allocate_server $1
+if [[$1 = "experiment"]]
+then
+    allocate_server
+elif [[$1 = "stats"]]
+then 
+    get_overall_stats
+fi
--- throughput.py
+++ throughput.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
-    stats = pd.read_csv('client.csv',dtype=str)
-    throughput = stats['measurement_c'].astype(float)
-    output = pd.DataFrame([[np.amax(throughput), np.amin(throughput), np.average(throughput)]], columns=['min_throughput', 'max_throughput', 'avg_throughput'])
-    output.to_csv('throughput.csv', index=False)
+    stats = pd.read_csv('client.csv',dtype=str, header=None, usecols=[3])
+    throughput = stats.astype(float)
+    output = pd.DataFrame([[np.amax(throughput).to_string(index=False).strip(), np.amin(throughput).to_string(index=False).strip(), np.average(throughput)]])
+    output.to_csv('throughput.csv', index=False, header=False)
