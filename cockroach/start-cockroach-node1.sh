7741cb058424a81ca9be4d463b35c2daac2acf49
kwekke <kwekke@u.nus.edu>
2021-11-01 21:37:04 +0800

    remove start-cockorach-node1.sh

--- cockroach/start-cockroach-node1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-server=$(hostname -s)
-echo "Starting cockroach on $server"
-
-ip=$(hostname -I)
-echo "IP is $ip"
-
-cockroach start --store=node1 --join=137.132.84.21 --advertise-addr=137.132.84.21 --cache=.25 --max-sql-memory=.25 --insecure
